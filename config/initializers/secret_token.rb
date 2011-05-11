245e28ca2fe0814ebde7c639fcf1a7811918d605
satish <satish.nagalla@gmail.com>
2011-05-11 18:17:52 +0530

    Initial commit - basic products table showing n editing n creating

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '45bd5f512e4eae907ed1e107ce7a9816bf419cd587ace24b12b8c42ad485dca9bb99d8519d013798cdee2d13497ad58d0ef118e19df71d881de4a311107325b8'
