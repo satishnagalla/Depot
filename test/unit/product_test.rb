a03ba0e2e7db9a9259f00b34bc29e9e65e4eaa90
satish <satish.nagalla@gmail.com>
2011-05-16 15:16:24 +0530

    Changing error messages to Activerecord error messgs

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -50,13 +50,13 @@ class ProductTest < ActiveSupport::TestCase
   end
 
 
-  test "product is not valid without a unique title" do
+  test "product is not valid without a unique title - i18n" do
   	product = Product.new(:title => products(:ruby).title,
 			:description  => "yyy",
 			:price  => 1,
 			:image_url  => "fred.gif")
 	assert !product.save
-	assert_equal "has already been taken", product.errors[:title].join('; ')
+	assert_equal I18n.translate('activerecord.errors.messages.taken') , product.errors[:title].join('; ')
   end
 
 
