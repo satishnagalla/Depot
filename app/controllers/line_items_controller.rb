a6f0fea950c9b47dd029549743c4d90306f2ca96
satish <satish.nagalla@gmail.com>
2011-05-18 05:18:18 +0530

    Removed multiple items in the cart n display 2 x item etc

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,7 +43,7 @@ class LineItemsController < ApplicationController
   #  @line_item = LineItem.new(params[:line_item])
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product => product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
