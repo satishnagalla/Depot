69ee4227954bf2d8f903376a13ddec12ae90eb26
satish <satish.nagalla@gmail.com>
2011-05-18 01:53:00 +0530

    Cart Creation n updating functional tests for it n adding Add to cart button

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,22 @@
 class Product < ActiveRecord::Base
-	default_scope :order => 'title'
-	validates :title, :description, :image_url, :presence => true
-	validates :title, :length => {:minimum => 10,:message => 'minimum length = 10 '}
-	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-	validates :title, :uniqueness => true
-	validates :image_url, :format => {
-			:with => %r{\.(gif|jpg|png)$}i,
-			:message => 'must be a URL for GIF, JPG or PNG image.'
-	}
+  default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
+  validates :title, :description, :image_url, :presence => true
+  validates :title, :length => {:minimum => 10,:message => 'minimum length = 10 '}
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
