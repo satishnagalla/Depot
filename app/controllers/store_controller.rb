9ec7fbed9c59a9076eb984dc1ca3770d92dd5703
satish <satish.nagalla@gmail.com>
2011-05-19 15:11:44 +0530

    Showing Cart in sidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @cart = current_cart
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @line_items }
