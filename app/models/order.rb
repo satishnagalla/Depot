df06a7e33c87fba5916917cf68f045da42972bf8
satish <satish.nagalla@gmail.com>
2011-05-23 14:07:56 +0530

    Capturing Order form user details n payment type

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ActiveRecord::Base
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  validates :name, :address, :email, :pay_type, :presence => true
+  validates :pay_type, :inclusion => PAYMENT_TYPES
+end
