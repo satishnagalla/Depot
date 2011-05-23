25f692b086f2b809afd6a29ab80a68431f280289
satish <satish.nagalla@gmail.com>
2011-05-23 16:17:44 +0530

    Capturing the Order Details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,14 +43,20 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+
   # POST /orders
   # POST /orders.xml
   def create
     @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(current_cart)
+
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to(@order, :notice => 'Order was successfully created.') }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to(store_url, :notice =>'Thank you for your order.') }
+#        format.html { redirect_to(@order, :notice => 'Order was successfully created.') }
         format.xml  { render :xml => @order, :status => :created, :location => @order }
       else
         format.html { render :action => "new" }
